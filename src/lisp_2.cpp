baa905bf20b88acc3cd56b2b59e6ea45d508fe15
ShunjiroOsada <tabletennis1216@gmail.com>
2018-07-13 17:50:49 +0000

    Changin lisp_2.cpp with using STL algorithm.

--- src/lisp_2.cpp
+++ src/lisp_2.cpp
@@ -4,77 +4,72 @@
 #include <string>
 #include <algorithm>
 
-void check_depth(std::string str)
+#define str_condition auto iter {str.begin()}; iter != str.end(); ++iter
+
+void introduction_sentence()
 {
-  for (auto iter {str.begin()}; iter != str.end(); ++iter)
+  std::cout <<"Welcom to Osada Lisp <https://github.com/ShunjiroOsada/lisp_interpreter>"<< std::endl;
+  std::cout << "This is Lisp interpreter by Osada" << std::endl;
+}
+
+void s_exp_analysis(std::string str)
+{
+  for (str_condition)
   {
-    if (std::)
+    if (*iter == '(' || *iter == ')')
+    {
+      std::cout << *iter << ", ";
+    }
+    if (*iter != '(' && *iter != ')' && *iter != ' ')
+    {
+      if (*(iter+1) != '(' && *(iter+1) != ')' && *(iter+1) != ' ')
+      {
+        std::cout << *iter;
+      }
+      else
+      {
+        std::cout << *iter << ", ";
+      }
+    }
   }
+  std::cout << std::endl;
+}
+
+//+ std::count(str.begin(), str.end(), ')')
+
+bool check_depth(std::string str)
+{
+  int depth = std::count(str.begin(), str.end(), '(') - std::count(str.begin(), str.end(), ')');
+  if (depth != 0)
+  {
+    std::cout << "This is not uncompleted s-expression." << std::endl;
+    std::cout << "depth: " << depth;
+    if (depth > 0)
+    {
+      std::cout << " --> There are " << depth << " more left-parentheses than right-parentheses in this s-expression." << std::endl;
+    }
+    else
+    {
+      std::cout << " --> There are " << -depth << " more right-parentheses than left-parentheses in this s-expression." << std::endl;
+    }
+    return false;
+  }
+  return true;
 }
 
 int main(void)
 {
-    std::cout << "This is Lisp interpreter by Osada" << std::endl;
-
-    while (true)
-    {
-        std::cout << ">>";
-        std::string s;
-        std::getline( std::cin, s );
-        if (s.front() != '(' || s.back() != ')') //checking first and end parentheses in s-expression
-        {
-            std::cout << "This is not s-expression!" << std::endl;
-            continue;
-        }
-
-        int depth = 0;
-
-        for (auto iter {s.begin()}; iter != s.end(); ++iter) //checking the number of parentheses in s-expression
-        {
-            if (*iter == '(')
-            {
-                ++depth;
-            } 
-            if (*iter == ')')
-            {
-                --depth;
-            }
-        }
-
-        if (depth != 0)
-        {
-            std::cout << "This is not uncompleted s-expression." << std::endl;
-            std::cout << "depth: " << depth;
-            if (depth > 0)
-            {
-                std::cout << " --There are " << depth << " more left-parentheses than right-parenthese in this s-expression." << std::endl;
-            }
-            if (depth < 0)
-            {
-                std::cout << " --There are " << -depth << " more right-parentheses than left-parentheses in this s-expression." << std::endl;
-            }
-            continue;
-        }
-
-        for (auto iter {s.begin()}; iter != s.end(); ++iter)
-        {
-            if (*iter == '(' || *iter == ')')
-            {
-                std::cout << *iter << ", ";
-            }
-            if (*iter != '(' && *iter != ')' && *iter != ' ')
-            {
-                if (*(iter+1) != '(' && *(iter+1) != ')' && *(iter+1) != ' ')
-                {
-                    std::cout << *iter;
-                }
-                else
-                {
-                    std::cout << *iter << ", ";
-                }
-            }
-        }
-        std::cout << std::endl;
-    }
-    return 0; 
+  introduction_sentence();
+  unsigned int count = 0;
+  unsigned int mis_count = 0;
+  while (true)
+  { 
+    ++count;
+    std::cout << '[' << count << ']' << '[' << mis_count << "]>";
+    std::string s;
+    std::getline( std::cin, s );
+    if (!check_depth(s)) { ++mis_count; continue; }
+    s_exp_analysis(s); 
+  }
+  return 0; 
 }
