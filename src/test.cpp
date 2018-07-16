adc9a817787421c9a8bef6185a7bde74f0c52e7d
Shunjiro Osada <tabletennis1216@gmail.com>
2018-07-17 02:29:31 +0900

    add a cpp-file "lisp_4.cpp" for uncompleted interpreter to calculate.

--- src/test.cpp
+++ src/test.cpp
@@ -3,93 +3,11 @@
 #include <string>
 #include <algorithm>
 #include <list>
-
-std::string eval(std::string str)
-{
-  std::list<std::string> ls; 
-  std::string oper, car, cdr;
-
-  auto iter {std::find_if_not(str.begin(), str.end(), [](char ch) { return ch == '('; })};
-
-  for (; *iter != ' '; ++iter)
-  {
-    oper += *iter;
-  }
-
-  if (oper != "+" && oper != "-" && oper != "*" && oper != "/") { return oper; }
-
-  auto iter_1 {std::find_if_not(iter, str.end(), [](char ch) { return ch == ' '; })};
-  if (*iter_1 == '(')
-  {
-    int count = 0;
-    for (; ; ++iter_1)
-    {
-      car += *iter_1;
-      if (*iter_1 == ')') { --count; }
-
-      if (*iter_1 == '(') { ++count; }
-
-      if (count == 0) { break; }
-    }
-  }
-  else
-  {
-    for (; *iter_1 != ' '; ++iter_1)
-    {
-      car += *iter_1;
-    }
-  }
-  auto iter_2 {std::find_if_not(iter_1, str.end(), [](char ch) { return ch == ' '; })};
-  if (*iter_2 == '(')
-  {
-    int count = 0;
-    for (; ; ++iter_2)
-    {
-      cdr += *iter_2;
-      if (*iter_2 == ')') { --count; }
-
-      if (*iter_2 == '(') { ++count; }
-
-      if (count == 0) { break; }
-    }
-  }
-  else
-  {
-    for (; *iter_2 != ')'; ++iter_2)
-    {
-      cdr += *iter_2;
-    }
-  }
-
-  double x = std::stod(eval(car));
-  double y = std::stod(eval(cdr));
-  
-  if (oper == "+")
-  { 
-    double val = x + y;
-    return { std::to_string(val) };
-  }
-  if (oper == "-")
-  {
-    double val = x - y;
-    return { std::to_string(val) };
-  }
-  if (oper == "*")
-  {
-    double val = x * y;
-    return { std::to_string(val) };
-  }
-  if (oper == "/")
-  {
-    double val = x / y;
-    return { std::to_string(val) };
-  }
-}
-
+#include <cctype>
 int main()
 {
-  std::string str= eval("(* 4 (/ 5 (+ 4 5)))");
-  std::cout << str << std::endl;
+  std::string str = "cde";
+  bool result = std::all_of(str.cbegin(), str.cend(), isalpha);
+  std::cout << result << std::endl;
   return 0;
-
 }
