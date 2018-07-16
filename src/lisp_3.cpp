e9675aec19549b489db9ae4df65f329efc15566a
Shunjiro Osada <tabletennis1216@gmail.com>
2018-07-17 00:59:38 +0900

    Change lisp_3.cpp to be enable to calculate four arithmetic operations in s-expression.

--- src/lisp_3.cpp
+++ src/lisp_3.cpp
@@ -2,36 +2,94 @@
 #include <cstdlib>
 #include <string>
 #include <algorithm>
+#include <list>
 
-void eval(std::string str)
+std::string eval(std::string str)
 {
+  std::list<std::string> ls; 
   std::string oper, car, cdr;
+
   auto iter {std::find_if_not(str.begin(), str.end(), [](char ch) { return ch == '('; })};
 
   for (; *iter != ' '; ++iter)
   {
     oper += *iter;
   }
-  std::cout << oper << std::endl;
+
+  if (oper != "+" && oper != "-" && oper != "*" && oper != "/") { return oper; }
 
   auto iter_1 {std::find_if_not(iter, str.end(), [](char ch) { return ch == ' '; })};
-  for (; *iter_1 != ' '; ++iter_1)
+  if (*iter_1 == '(')
   {
-    car += *iter_1;
+    int count = 0;
+    for (; ; ++iter_1)
+    {
+      car += *iter_1;
+      if (*iter_1 == ')') { --count; }
+
+      if (*iter_1 == '(') { ++count; }
+
+      if (count == 0) { break; }
+    }
   }
-  std::cout << car << std::endl;
+  else
+  {
+    for (; *iter_1 != ' '; ++iter_1)
+    {
+      car += *iter_1;
+    }
+  }
   auto iter_2 {std::find_if_not(iter_1, str.end(), [](char ch) { return ch == ' '; })};
-  for (; iter_2 != str.end(); ++iter_2)
+  if (*iter_2 == '(')
   {
-    cdr += *iter_2;
+    int count = 0;
+    for (; ; ++iter_2)
+    {
+      cdr += *iter_2;
+      if (*iter_2 == ')') { --count; }
+
+      if (*iter_2 == '(') { ++count; }
+
+      if (count == 0) { break; }
+    }
   }
-  std::cout << cdr << std::endl;
+  else
+  {
+    for (; *iter_2 != ')'; ++iter_2)
+    {
+      cdr += *iter_2;
+    }
+  }
+
+  double x = std::stod(eval(car));
+  double y = std::stod(eval(cdr));
+  
+  if (oper == "+")
+  { 
+    double val = x + y;
+    return { std::to_string(val) };
+  }
+  if (oper == "-")
+  {
+    double val = x - y;
+    return { std::to_string(val) };
+  }
+  if (oper == "*")
+  {
+    double val = x * y;
+    return { std::to_string(val) };
+  }
+  if (oper == "/")
+  {
+    double val = x / y;
+    return { std::to_string(val) };
+  }
 }
 
 int main()
 {
-  eval("(+ dddddddd ())");
-  
+  std::string str= eval("(* 4 (/ 5 (+ 4 5)))");
+  std::cout << str << std::endl;
   return 0;
 
 }
--- src/test.cpp
+++ src/test.cpp
@@ -2,15 +2,94 @@
 #include <cstdlib>
 #include <string>
 #include <algorithm>
+#include <list>
+
+std::string eval(std::string str)
+{
+  std::list<std::string> ls; 
+  std::string oper, car, cdr;
+
+  auto iter {std::find_if_not(str.begin(), str.end(), [](char ch) { return ch == '('; })};
+
+  for (; *iter != ' '; ++iter)
+  {
+    oper += *iter;
+  }
+
+  if (oper != "+" && oper != "-" && oper != "*" && oper != "/") { return oper; }
+
+  auto iter_1 {std::find_if_not(iter, str.end(), [](char ch) { return ch == ' '; })};
+  if (*iter_1 == '(')
+  {
+    int count = 0;
+    for (; ; ++iter_1)
+    {
+      car += *iter_1;
+      if (*iter_1 == ')') { --count; }
+
+      if (*iter_1 == '(') { ++count; }
+
+      if (count == 0) { break; }
+    }
+  }
+  else
+  {
+    for (; *iter_1 != ' '; ++iter_1)
+    {
+      car += *iter_1;
+    }
+  }
+  auto iter_2 {std::find_if_not(iter_1, str.end(), [](char ch) { return ch == ' '; })};
+  if (*iter_2 == '(')
+  {
+    int count = 0;
+    for (; ; ++iter_2)
+    {
+      cdr += *iter_2;
+      if (*iter_2 == ')') { --count; }
+
+      if (*iter_2 == '(') { ++count; }
+
+      if (count == 0) { break; }
+    }
+  }
+  else
+  {
+    for (; *iter_2 != ')'; ++iter_2)
+    {
+      cdr += *iter_2;
+    }
+  }
+
+  double x = std::stod(eval(car));
+  double y = std::stod(eval(cdr));
+  
+  if (oper == "+")
+  { 
+    double val = x + y;
+    return { std::to_string(val) };
+  }
+  if (oper == "-")
+  {
+    double val = x - y;
+    return { std::to_string(val) };
+  }
+  if (oper == "*")
+  {
+    double val = x * y;
+    return { std::to_string(val) };
+  }
+  if (oper == "/")
+  {
+    double val = x / y;
+    return { std::to_string(val) };
+  }
+}
 
 int main()
 {
-  std::string str = "abcdefg";
-  std::string test;
-  for (auto iter {str.begin()}; iter != str.end(); ++iter)
-  {
-    test += *iter;
-  }
+  std::string str= eval("(* 4 (/ 5 (+ 4 5)))");
   std::cout << str << std::endl;
   return 0;
+
 }
